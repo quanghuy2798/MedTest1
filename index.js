4364500c3a579af2bb95bdf3c6dce3a02ef6005d
quanghuy2798 <53011883+quanghuy2798@users.noreply.github.com>
2020-02-26 21:40:16 +0700

    Revert "Upload source code"
    This reverts commit 339b22bab4b8834b570087b773f86d5b527c8cc6.

--- index.js
+++
@@ -1,8 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
-import Login from './Component/Login'
-import ForgottenPassword from './Component/ForgottenPassword'
-import Register from './Component/Register'
-
-AppRegistry.registerComponent(appName, () => Register );
